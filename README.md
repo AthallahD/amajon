015e7e9592e890e96f6d3a8749e09c0fd2eb1710
AthallahD <athallah2209@gmail.com>
2024-09-09 21:37:27 +0700

    updating views.py, README.md and main.html

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas secara step-by-step (bukan hanya sekadar mengikuti tutorial).
-Buatlah bagan yang berisi request client ke web aplikasi berbasis Django beserta responnya dan jelaskan pada bagan tersebut kaitan antara urls.py, views.py, models.py, dan berkas html.
-Jelaskan fungsi git dalam pengembangan perangkat lunak!
-Menurut Anda, dari semua framework yang ada, mengapa framework Django dijadikan permulaan pembelajaran pengembangan perangkat lunak?
-Mengapa model pada Django disebut sebagai ORM?
+## Questions
+
+1. Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas secara step-by-step (bukan hanya sekadar mengikuti tutorial).
+2. Buatlah bagan yang berisi request client ke web aplikasi berbasis Django beserta responnya dan jelaskan pada bagan tersebut kaitan antara urls.py, views.py, models.py, dan berkas html.
+3. Jelaskan fungsi git dalam pengembangan perangkat lunak!
+4. Menurut Anda, dari semua framework yang ada, mengapa framework Django dijadikan permulaan pembelajaran 
+5. pengembangan perangkat lunak?
+6. Mengapa model pada Django disebut sebagai ORM?
+
+## Answers
+1. - Membuat Direktori dan mengaktifkan virtual environment
+   - Menyiapkan dependencies yang dibutuhkan dan membuat proyek Django
+   - Konfigurasi proyek dan menjalankan server
+   - 
+2. 
+3. - Fungsi git dalam pengembangan perangkat lunak sendiri adalah sebagai version control system. Fungsi utamanya
+     adalah melacak perubahan kode sehingga dapat menyimpan versi-versi berbeda dari proyek tanpa harus menyimpan file duplikat secara manual.
+
+4. - Framework Django sebagai permulaan pembelajaran adalah karena kemudahan dalam penggunaan terutama dalam penggunaan bahasa pemrograman Python. Selain itu pada Django terdapat banyak sekali fitur "Built-in" yang dapat dieksplor oleh pengguna. Pada Django sendiri, pola yang digunakan adalah MVT atau Model View Template yang membantu membagi tugas dalam aplikasi web dengan terstruktur dan logis.
+
+5. - Model pada Django disebut ORM (Object-Relational Mapping) karena berfungsi sebagai penghubung antara objek di dalam kode Python dengan tabel di dalam database relasional. ORM mempermudah pekerjaan user dengan database tanpa perlu menulis SQL secara langsung sehingga user bisa berinteraksi dengan database menggunakan objek Python yang lebih intuitif.
--- main/views.py
+++ main/views.py
@@ -1,59 +1,11 @@
 from django.shortcuts import render
 
 def show_main(request):
-    products = [
-        {
-            'name' : 'Ryzen 3 3200G',
-            'price': 1500000,
-            'description': 'Ryzen 3 3200G is a processor that has 4 cores and 4 threads. It has a base clock of 3.6 GHz and a boost clock of 4.0 GHz. It is a good processor for gaming and productivity tasks.',
-            'stock': 10
-        },
-        {
-            'name' : 'Ryzen 5 3600',
-            'price': 2500000,
-            'description': 'Ryzen 5 3600 is a processor with 6 cores and 12 threads. It has a base clock of 3.6 GHz and a boost clock of 4.2 GHz.',
-            'stock': 5
-        },
-        {
-            'name' : 'Ryzen 7 3700X',
-            'price': 4500000,
-            'description': 'Ryzen 7 3700X has 8 cores and 16 threads with a base clock of 3.6 GHz and a boost clock of 4.4 GHz, suitable for high-end gaming and productivity.',
-            'stock': 3
-        },
-        {
-            'name' : 'Ryzen 5 5600',
-            'price': 3500000,
-            'description': 'Ryzen 5 5600 is a processor with 6 cores and 12 threads. It has a base clock of 3.7 GHz and a boost clock of 4.6 GHz.',
-            'stock': 7
-        },
-        {
-            'name' : 'Ryzen 9 5900X',
-            'price': 7500000,
-            'description': 'Ryzen 9 5900X has 12 cores and 24 threads with a base clock of 3.7 GHz and a boost clock of 4.8 GHz, suitable for high-end gaming and productivity.',
-            'stock': 2
-        },
-        {
-            'name' : 'Ryzen 9 5950X',
-            'price': 9500000,
-            'description': 'Ryzen 9 5950X has 16 cores and 32 threads with a base clock of 3.4 GHz and a boost clock of 4.9 GHz, suitable for high-end gaming and productivity.',
-            'stock': 1
-        },
-        {
-            'name' : 'Ryzen Threadripper 3960X',
-            'price': 15000000,
-            'description': 'Ryzen Threadripper 3960X has 24 cores and 48 threads with a base clock of 3.8 GHz and a boost clock of 4.5 GHz, suitable for high-end gaming and productivity.',
-            'stock': 1
-        },
-        {
-            'name' : 'Ryzen Threadripper 3970X',
-            'price': 20000000,
-            'description': 'Ryzen Threadripper 3970X has 32 cores and 64 threads with a base clock of 3.7 GHz and a boost clock of 4.5 GHz, suitable for high-end gaming and productivity.',
-            'stock': 1
-        }
-    ]
-
     context = {
-        'products': products
+        'name' : 'Ryzen 3 3200G',
+        'price': 1500000,
+        'description': 'Ryzen 3 3200G is a processor that has 4 cores and 4 threads. It has a base clock of 3.6 GHz and a boost clock of 4.0 GHz. It is a good processor for gaming and productivity tasks.',
+        'stock': 10
     }
 
     return render(request, "main.html", context)
