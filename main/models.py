286783e598bfa295346d520abbcc3014761a462c
AthallahD <athallah2209@gmail.com>
2024-09-09 21:54:09 +0700

    updating views, models, and main

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Goods(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    stock = models.IntegerField()
+    quantity = models.IntegerField()
 
     def __str__(self):
         return print(f"Name: {self.name}, Price: {self.price}, Stock: {self.stock}")
--- main/views.py
+++ main/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name' : 'Ryzen 3 3200G',
-        'price': 1500000,
-        'description': 'Ryzen 3 3200G is a processor that has 4 cores and 4 threads. It has a base clock of 3.6 GHz and a boost clock of 4.0 GHz. It is a good processor for gaming and productivity tasks.',
-        'stock': 10
+        'application_name' : 'Amajon',
+        'nama': 'Athallah Damar Jiwanto',
+        'npm': '2306245024',
+        'kelas': 'PBP D',
     }
 
     return render(request, "main.html", context)
