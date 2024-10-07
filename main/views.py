f393aea4fd03b5515aea0d12fcd7af8eb1c8253b
AthallahD <athallah2209@gmail.com>
2024-10-07 11:37:00 +0700

    feat : updating urls.py, views.py and main.html

--- main/views.py
+++ main/views.py
@@ -10,17 +10,16 @@ from django.contrib.auth import login, logout, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
-
     context = {
         'application_name' : 'Amajon',
         'nama': request.user.username,
         'npm': '2306245024',
         'kelas': 'PBP D',
-        'products': product_entries,
         'last_login': request.COOKIES.get('last_login'),
     }
 
@@ -37,6 +36,24 @@ def create_product_entry(request):
 
     context = {'form': form}
     return render(request, "create_product_entry.html", context)
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = request.POST.get("name")
+    price = request.POST.get("price")
+    description = request.POST.get("description")
+    quantity = request.POST.get("quantity")
+    user = request.user
+
+    new_product = ProductForm(
+        name=name, price=price,
+        description=description, quantity=quantity,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
 
 def register(request):
     form = UserCreationForm()
@@ -60,9 +77,13 @@ def login_user(request):
         response = HttpResponseRedirect(reverse("main:show_main"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
+      
+      else:
+        messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
+
    context = {'form': form}
    return render(request, 'login.html', context)
 
@@ -89,11 +110,11 @@ def delete_product(request, id):
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = ProductForm.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = ProductForm.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
